04720091d2844992a303425cb511cb845fafafa2
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-03-16 16:51:46 +0300

    Completed Assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -30,6 +30,7 @@ class BirdsController < ApplicationController
       render json: {error: "bird not found"} , status: :not_found
     end
   end
+  
   def increment_likes 
     bird= Bird.find_by(id: params[:id])
     if bird
